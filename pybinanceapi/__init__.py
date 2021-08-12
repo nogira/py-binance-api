5383318af3ea36f69de3921ce3da14cb84cffe6f
nogira <83847986+nogira@users.noreply.github.com>
2021-08-12 16:37:46 +1000

    minor tweaks

--- pybinanceapi/__init__.py
+++ pybinanceapi/__init__.py
@@ -7,18 +7,18 @@ from . import generated as generated
 
 def vars(baseurl='', api_key='', api_secret='', email=''):
     """
-## baseurl
-Possible Values:
-- https://api.binance.com
-- https://api1.binance.com
-- https://api2.binance.com
-- https://api3.binance.com
-- https://testnet.binance.vision
+    ## baseurl
+    Possible Values:
+    - https://api.binance.com
+    - https://api1.binance.com
+    - https://api2.binance.com
+    - https://api3.binance.com
+    - https://testnet.binance.vision
 
-## email
-- Must replace '@' with '%40' 
+    ## email
+    - Must replace '@' with '%40' 
     """
-    if baseurl != '': generated.baseurl = baseurl
-    if api_key != '': generated.api_key = api_key
-    if api_secret != '': generated.api_secret = api_secret
-    if email != '': generated.email = email
+    if baseurl: generated.baseurl = baseurl
+    if api_key: generated.api_key = api_key
+    if api_secret: generated.api_secret = api_secret
+    if email: generated.email = email
